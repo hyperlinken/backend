e4373160ee05db0cb2b367408b765aee7eac8680
hyperlinken <154810014+hyperlinken@users.noreply.github.com>
2025-01-21 02:05:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app= express();
 const server=http.createServer(app);
 
 
-const io= socketIo(server);
+// const io= socketIo(server);
 
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
